d40fb79ae54157733c705a09bbacb6437c799c09
pho9902 <bagh9902@gmail.com>
2026-03-04 12:38:08 +0900

    chore: 폴더구조 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import AppPage from "./pages/AppPage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AppPage />
   </StrictMode>,
 );

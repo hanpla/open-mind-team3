01348d80ee344511417d6c61177611f8a7e020f8
Hanpla <hanpla25@gmail.com>
2026-03-07 12:23:20 +0900

    refactor: min-width 기반 모바일 퍼스트 방식으로 리팩토링

--- src/styles/media.js
+++ src/styles/media.js
@@ -1,22 +1,21 @@
 import { css } from "styled-components";
 
 const breakpoints = {
-  mobile: "375px",
   tablet: "768px",
   pc: "1200px",
 };
 
 export const media = {
   mobile: (...args) => css`
-    @media (min-width: ${breakpoints.mobile}) and (max-width: 767px) {
-      ${css(...args)};
-    }
+    ${css(...args)};
   `,
+
   tablet: (...args) => css`
-    @media (min-width: ${breakpoints.tablet}) and (max-width: 1199px) {
+    @media (min-width: ${breakpoints.tablet}) {
       ${css(...args)};
     }
   `,
+
   pc: (...args) => css`
     @media (min-width: ${breakpoints.pc}) {
       ${css(...args)};

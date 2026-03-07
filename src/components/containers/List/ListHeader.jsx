e001809926f1487df869547f35c2be331ee3c5c4
Hanpla <hanpla25@gmail.com>
2026-03-07 15:53:08 +0900

    chore: 버튼 경로 수정

--- src/components/containers/List/ListHeader.jsx
+++ src/components/containers/List/ListHeader.jsx
@@ -1,25 +1,19 @@
-import { SecondButton } from "@/components/common/Button";
-import * as S from "@/components/containers/List/ListPage.style";
-import logo from "@/assets/icons/logo.svg";
 import { useNavigate } from "react-router-dom";
 
-export default function ListHeader(){
-    const navigate = useNavigate();
-    const handleGoAnswer = () => {
+import logo from "@/assets/icons/logo.svg";
+import * as S from "@/components/containers/List/ListPage.style";
+import { SecondButton } from "@/components/common/Button/Button.style";
+
+export default function ListHeader() {
+  const navigate = useNavigate();
+  const handleGoAnswer = () => {
     const myFeedId = localStorage.getItem("myFeedId");
     navigate(myFeedId ? `/post/${myFeedId}/answer` : "/");
   };
-  return(
-          <S.Header>
-            <S.LogoImg 
-              src={logo}
-              alt="OpenMind" 
-              onClick={() => navigate("/")} 
-            />
-            <SecondButton onClick={handleGoAnswer}>답변하러 가기 →</SecondButton>
-          </S.Header>
-  )
-
-
-
+  return (
+    <S.Header>
+      <S.LogoImg src={logo} alt="OpenMind" onClick={() => navigate("/")} />
+      <SecondButton onClick={handleGoAnswer}>답변하러 가기 →</SecondButton>
+    </S.Header>
+  );
 }

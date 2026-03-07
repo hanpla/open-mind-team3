ba203964cca66112a1d0a71ee5fd729356f6d8ad
Hanpla <hanpla25@gmail.com>
2026-03-07 11:20:25 +0900

    chore: 예시 코드 복원

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,3 @@
-import { theme } from "@/styles/theme";
-import { openToast } from "@/utils/toast";
-
 export default function HomePage() {
-  const handleBtnClick = () => {
-    openToast("토스트 출력", theme.colors.red);
-  };
-
-  return (
-    <div>
-      <button onClick={handleBtnClick}>토스트 클릭</button>
-    </div>
-  );
+  return <div>HomePage</div>;
 }
